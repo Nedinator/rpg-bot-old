d657c96f5a20568223ca526fa38ded5c2f921a36
Nedinator <imnedinator@gmail.com>
2018-11-19 11:36:25 -0600

    idek what i did lol

--- commands/battle.js
+++ commands/battle.js
@@ -4,8 +4,6 @@ mongoose.connect('mongodb://localhost/nedrpg', {
   useNewUrlParser: true
 });
 const Coins = require("../models/coins.js");
-
-
 module.exports.run = async (bot, message, args) => {
   //this is where the actual code for the command goes
   await message.delete();
@@ -13,26 +11,22 @@ module.exports.run = async (bot, message, args) => {
   if (!target) return message.reply("Please mention a member to battle!").then(r => r.delete(10000));
   let price = parseInt(args[1]);
   if (!price || isNaN(price) || price < 1) return message.reply("Please try that again... ").then(r => r.delete(10000));
-
   let embed = new Discord.RichEmbed()
     .setTitle("Battle!")
     .setColor("BLURPLE")
     .setDescription(price + " coins at stake");
-
   Coins.findOne({
     userID: message.author.id,
     serverID: message.guild.id
   }, (err, res) => {
     if (err) console.log(err);
     if (!res || res.coins < price) return message.reply("Sorry but you dont have enough coins for that").then(r => r.delete(10000));
-
     Coins.findOne({
       userID: target.id,
       serverID: message.guild.id
     }, (err, targetres) => {
       if (err) console.log(err)
       if (!targetres || targetres.coins < price) return message.reply("Sorry but the target doesn't have enough coins for that").then(r => r.delete(10000));
-
       const filter = m => m.author.id === target.id;
       message.channel.send(target + " you have been challenged by " + message.author + " for " + price + " coins. To accept type 'accept'. You have 20 seconds").then(r => r.delete(20000));
       message.channel.awaitMessages(filter, {
@@ -56,25 +50,15 @@ module.exports.run = async (bot, message, args) => {
             embed.addField("Loser", message.author);
           }
           //end
-
           targetres.save().catch(err => console.log(err));
           res.save().catch(err => console.log(err));
           message.channel.send(embed);
         }
       }).catch(err => {
         message.reply(" - " + target + " Time limit exceeded").then(r => r.delete(5000));
-      })
-
-    })
-
-
-
+      });
+    });
   });
-
-  //message await stuff that needs to happen pretty much last
-
-
-
 }
 //name this whatever the command name is.
 module.exports.help = {
