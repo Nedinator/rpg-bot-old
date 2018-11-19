7f65aa3580b679a83b3cce1ad7d1223f0ee412e1
Nedinator <imnedinator@gmail.com>
2018-11-19 10:55:45 -0600

    help is finished. will continue updating added shop. nothing here yet boys

--- commands/help.js
+++ commands/help.js
@@ -5,15 +5,14 @@ module.exports.run = async (bot, message, args) => {
   //this is where the actual code for the command goes
   await message.delete(50);
 
-    let embed = new Discord.RichEmbed()
+  let embed = new Discord.RichEmbed()
     .setTitle("Help Menu")
     .setColor("BLURPLE") //who doesn't love a good blurple
     .setThumbnail(bot.user.displayAvatarURL)
-    .setDescription("Current version: v" + pkg.version + ". Prefix: "+ conf.prefix)
+    .setDescription("Current version: v" + pkg.version + ". Prefix: " + conf.prefix)
     .addField("Current active", "battle, coins, help, pay, wealthy")
     .addField("Coming Soon", "profile, shop, level, class");
-  
-  
+
   message.channel.send(embed)
 }
 //name this whatever the command name is.
---
+++ commands/shop.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+    //this is where the actual code for the command goes
+
+}
+//name this whatever the command name is.
+module.exports.help = {
+    name: 'shop'
+}
