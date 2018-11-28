9d0c5ac118edeb84ddfe3f7b8476be9397925bbb
Roy <root@mrsheldon.me>
2018-11-28 20:11:29 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,21 +1,23 @@
 const Discord = require('discord.js')
 const pkg = require("../package.json");
 const conf = require("../config.json");
+
 module.exports.run = async (bot, message, args) => {
-  //this is where the actual code for the command goes
-  await message.delete(50);
+    //this is where the actual code for the command goes
+    await message.delete(50);
 
-  let embed = new Discord.RichEmbed()
-    .setTitle("Help Menu")
-    .setColor("BLURPLE") //who doesn't love a good blurple
-    .setThumbnail(bot.user.displayAvatarURL)
-    .setDescription("Current version: v" + pkg.version + ". Prefix: " + conf.prefix)
-    .addField("Current active", "battle, coins, help, pay, wealthy, getpet, mypet")
-    .addField("Coming Soon", "profile, shop, level, class, trainpet, battlepet");
+    let embed = new Discord.RichEmbed()
+        .setTitle("Help Menu")
+        .setColor("BLURPLE") 
+        .setThumbnail(bot.user.displayAvatarURL)
+        .setDescription("Current version: v" + pkg.version + ". Prefix: " + conf.prefix)
+        .addField("Current active", "battle, coins, help, pay, wealthy, getpet, mypet")
+        .addField("Coming Soon", "profile, shop, level, class, trainpet, battlepet");
 
-  message.channel.send(embed)
+    message.channel.send(embed)
 }
+
 //name this whatever the command name is.
 module.exports.help = {
-  name: 'help'
+    name: 'help'
 }
