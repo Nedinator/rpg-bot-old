dc25d114cf60633a77234ead2754f1a329645847
Roy <root@mrsheldon.me>
2018-11-28 20:10:30 +0200

    Update mypet.js

--- commands/mypet.js
+++ commands/mypet.js
@@ -1,18 +1,19 @@
 const Discord = require('discord.js')
 const Pet = require("../models/pets.js")
+
 module.exports.run = async (bot, message, args) => {
     //this is where the actual code for the command goes
     Pet.findOne({
         ownerID: message.author.id
     }, (err, res) => {
         let embed = new Discord.RichEmbed()
-        .setTitle("My Pet!")
-        .setThumbnail(message.author.displayAvatarURL)
+            .setTitle("My Pet!")
+            .setThumbnail(message.author.displayAvatarURL)
 
-        if(!res){
+        if (!res) {
             embed.setColor("RED")
             embed.addField("Sorry!", "You don't have a pet... Please type !getpet.")
-        }else{
+        } else {
             embed.setColor("BLURPLE");
             embed.addField("Pet Name", res.petName);
             embed.addField("Pet Level", "Level: " + res.petLvl + ".");
