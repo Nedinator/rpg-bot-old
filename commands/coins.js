833015d1eb9085ebbcd0e81c42a0fc73e8f49238
Roy <root@mrsheldon.me>
2018-11-28 20:11:53 +0200

    Update coins.js

--- commands/coins.js
+++ commands/coins.js
@@ -1,39 +1,40 @@
 const Discord = require('discord.js')
 const mongoose = require("mongoose")
-mongoose.connect('mongodb://localhost/nedrpg', {
-  useNewUrlParser: true
-});
 const Coins = require("../models/coins.js");
 
+mongoose.connect('mongodb://localhost/nedrpg', {
+    useNewUrlParser: true
+});
 
 module.exports.run = async (bot, message, args) => {
-  //this is where the actual code for the command goes
-  await message.delete()
+    //this is where the actual code for the command goes
+    await message.delete()
 
-  let embed = new Discord.RichEmbed()
-    .setTitle("Coins")
-    .setThumbnail(message.author.displayAvatarURL);
+    let embed = new Discord.RichEmbed()
+        .setTitle("Coins")
+        .setThumbnail(message.author.displayAvatarURL);
 
-  Coins.findOne({
-    userID: message.author.id,
-    serverID: message.guild.id
-  }, (err, res) => {
-    if(err) console.log(err);
+    Coins.findOne({
+        userID: message.author.id,
+        serverID: message.guild.id
+    }, (err, res) => {
+        if (err) console.log(err);
 
-    if(!res){
-      embed.setColor("RED");
-      embed.addField("Error", "Sorry, you don't have any coins in this server...");
-    }else{
-      embed.setColor("BLURPLE");
-      embed.addField(res.username, res.coins + " coins.");
-    }
+        if (!res) {
+            embed.setColor("RED");
+            embed.addField("Error", "Sorry, you don't have any coins in this server...");
+        } else {
+            embed.setColor("BLURPLE");
+            embed.addField(res.username, res.coins + " coins.");
+        }
 
-    message.channel.send(embed)
+        message.channel.send(embed)
 
-  })
+    })
 
 }
+
 //name this whatever the command name is.
 module.exports.help = {
-  name: "coins"
+    name: "coins"
 }
